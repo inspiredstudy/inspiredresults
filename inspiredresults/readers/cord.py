ce6185e8d9c5a3454a21e2851b9a2bd954d4879a
Jon Stutters <j.stutters@ucl.ac.uk>
2019-04-04 12:10:54 +0100

    Add cord measures and change test codes

--- inspiredresults/readers/cord.py
+++ inspiredresults/readers/cord.py
@@ -1,129 +1,93 @@
-import xlrd
-
-
-GM_CSA_MEAN_PATH = "/trials/INSPIRED/results/sct/GM_CSA/csa_mean.xls"
-WM_CSA_MEAN_PATH = "/trials/INSPIRED/results/sct/WM_CSA/csa_mean.xls"
-FA_IN_WM_PATH = "/trials/INSPIRED/results/sct/diffusion/FA_in_WM.xls"
-FA_IN_DC_PATH = "/trials/INSPIRED/results/sct/diffusion/FA_in_DC.xls"
-FA_IN_LF_PATH = "/trials/INSPIRED/results/sct/diffusion/FA_in_LF.xls"
-FA_IN_VF_PATH = "/trials/INSPIRED/results/sct/diffusion/FA_in_VF.xls"
+from inspiredresults.readers.helpers import path_to
+from csv import DictReader
+import pandas as pd
 
 
 def gm_csa_mean(subject, visit):
-    wb = xlrd.open_workbook(GM_CSA_MEAN_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[6])
-    return "MISSING"
+    csa_path = path_to(subject, visit) / "cord/sct_processing/t2s/gm_csa.csv"
+    csa_read = DictReader(csa_path.open("r"))
+    row = next(csa_read)
+    mean_area = row["MEAN(CSA [mm^2])"]
+    return "{0:0.2f}".format(float(mean_area))
 
 
 def wm_csa_mean(subject, visit):
-    wb = xlrd.open_workbook(WM_CSA_MEAN_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[6])
-    return "MISSING"
+    csa_path = path_to(subject, visit) / "cord/sct_processing/t2s/wm_csa.csv"
+    csa_read = DictReader(csa_path.open("r"))
+    row = next(csa_read)
+    mean_area = row["MEAN(CSA [mm^2])"]
+    return "{0:0.2f}".format(float(mean_area))
+
+
+def _dwi_read(subject, visit, contrast, tract):
+    filename = "{0}_in_{1}.csv".format(contrast.upper(), tract.upper())
+    read_path = path_to(subject, visit) / "cord/sct_processing/dwi" / filename
+    df = pd.read_csv(read_path)
+    res = df["MAP()"].mean()
+    if contrast in ["AD", "RD", "MD"]:
+        res *= 1000.0
+    return "{0:0.2f}".format(res)
 
 
 def fa_in_wm_mean(subject, visit):
-    wb = xlrd.open_workbook(FA_IN_WM_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[8])
-    return "MISSING"
+    return _dwi_read(subject, visit, "FA", "WM")
 
 
 def fa_in_dc_mean(subject, visit):
-    wb = xlrd.open_workbook(FA_IN_DC_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[8])
-    return "MISSING"
+    return _dwi_read(subject, visit, "FA", "DC")
 
 
 def fa_in_lf_mean(subject, visit):
-    wb = xlrd.open_workbook(FA_IN_LF_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[8])
-    return "MISSING"
+    return _dwi_read(subject, visit, "FA", "LF")
 
 
 def fa_in_vf_mean(subject, visit):
-    wb = xlrd.open_workbook(FA_IN_VF_PATH)
-    sheet = wb.sheet_by_index(0)
-    row = 0
-    while True:
-        row += 1
-        try:
-            cells = sheet.row_values(row)
-        except IndexError:
-            break
-        if not cells[1].startswith("/mounts/auto/trials/INSPIRED/scans"):
-            continue
-        site, type_, id_, row_visit = cells[1].split("/")[6:10]
-        site = int(site)
-        id_ = int(id_)
-        if site == subject.site and id_ == subject.id and type_ == subject.type and visit == row_visit:
-            return "{0:0.2f}".format(cells[8])
-    return "MISSING"
+    return _dwi_read(subject, visit, "FA", "VF")
+
+
+def md_in_wm_mean(subject, visit):
+    return _dwi_read(subject, visit, "MD", "WM")
+
+
+def md_in_dc_mean(subject, visit):
+    return _dwi_read(subject, visit, "MD", "DC")
+
+
+def md_in_lf_mean(subject, visit):
+    return _dwi_read(subject, visit, "MD", "LF")
+
+
+def md_in_vf_mean(subject, visit):
+    return _dwi_read(subject, visit, "MD", "VF")
+
+
+def ad_in_wm_mean(subject, visit):
+    return _dwi_read(subject, visit, "AD", "WM")
+
+
+def ad_in_dc_mean(subject, visit):
+    return _dwi_read(subject, visit, "AD", "DC")
+
+
+def ad_in_lf_mean(subject, visit):
+    return _dwi_read(subject, visit, "AD", "LF")
+
+
+def ad_in_vf_mean(subject, visit):
+    return _dwi_read(subject, visit, "AD", "VF")
+
+
+def rd_in_wm_mean(subject, visit):
+    return _dwi_read(subject, visit, "RD", "WM")
+
+
+def rd_in_dc_mean(subject, visit):
+    return _dwi_read(subject, visit, "RD", "DC")
+
+
+def rd_in_lf_mean(subject, visit):
+    return _dwi_read(subject, visit, "RD", "LF")
+
+
+def rd_in_vf_mean(subject, visit):
+    return _dwi_read(subject, visit, "RD", "VF")
